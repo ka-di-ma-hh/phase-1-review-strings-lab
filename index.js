8128783faaa9d9146dc55b1323bcbc9b3af072d3
junne.wanja@student.moringaschool.com <junne.wanja@student.moringaschool.com>
2024-07-11 09:11:50 +0300

    completestringslab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code in this file!
+
+var  currentUser = "Grace Hopper"
+
+var welcomeMessage = "Welcome to Flatbook!"
+
+// let currentUser = "Grace Hopper"
+
+  var welcomeMessage = "Grace Hopper!"
+  var welcomeMessage = "Welcome to Flatbook, Grace Hopper!"
+
+ var excitedWelcomeMessage = "Grace Hopper!"
+ var excitedWelcomeMessage = 'WELCOME TO FLATBOOK, '
+ var excitedWelcomeMessage = 'WELCOME TO FLATBOOK, GRACE HOPPER!'
+ let shortGreeting = 'Welcome, Grace!'
+ 
+
